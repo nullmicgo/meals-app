adaa80500b9f41d7986e0e169d15f9567c38aa34
nullmicgo <nullmicog@gmail.com>
2019-11-20 11:35:32 +0800

    Adding Favourite and filter function

--- screens/CategoriesMealScreen.js
+++ screens/CategoriesMealScreen.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
-
-import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
-const CategoriesMealScreen = props => {
-  
+const CategoryMealScreen = props => {
   const catId = props.navigation.getParam('categoryId');
 
   const availableMeals = useSelector(state => state.meals.filteredMeals);
@@ -16,10 +15,18 @@ const CategoriesMealScreen = props => {
     meal => meal.categoryIds.indexOf(catId) >= 0
   );
 
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals found, maybe check your filters?</DefaultText>
+      </View>
+    );
+  }
+
   return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
 
-CategoriesMealScreen.navigationOptions = navigationData => {
+CategoryMealScreen.navigationOptions = navigationData => {
   const catId = navigationData.navigation.getParam('categoryId');
 
   const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
@@ -29,4 +36,12 @@ CategoriesMealScreen.navigationOptions = navigationData => {
   };
 };
 
-export default CategoriesMealScreen;
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default CategoryMealScreen;
--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,32 +1,49 @@
 import React from 'react';
-
-import MealList from '../components/MealList';
-import { MEALS } from '../data/dummy-data';
+import { View, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import HeaderButton from '../components/HeaderButton';
 import { useSelector } from 'react-redux';
 
-const FavoritesScreen = props => {
+import HeaderButton from '../components/HeaderButton';
+import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
-  const favMeals = useSelector( state => state.meals.favoriteMeals);
-  //hard code tesintg only
-  // const favMeals = availableMeals.filter(meal => meal.id === 'm1' || meal.id === 'm2');
+const FavoritesScreen = props => {
+  const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favorite meals found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
+
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
-
 
 FavoritesScreen.navigationOptions = navData => {
   return {
     headerTitle: 'Your Favorites',
     headerLeft: (
-              <HeaderButtons HeaderButtonComponent={HeaderButton} >
-                  <Item title="Menu" iconName="ios-menu" onPress={()=>{
-                          navData.navigation.openDrawer();
-                  }} />
-          </HeaderButtons>
-      )
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
+};
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
-}
+});
 
-
-export default FavoritesScreen; 
+export default FavoritesScreen;
